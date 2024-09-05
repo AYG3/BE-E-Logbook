7a82baa33262d01c260399775792eeb6d644683b
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-05 09:42:00 +0100

    refactor: Update getUserLogbook route to use request parameters instead of request body

--- routes/logbookRoutes.js
+++ routes/logbookRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createLogbook, updateUserLogbook, getUserLogbooks, deleteUserLogbook } from "../controller/logbookController.js";
+import { createLogbook, updateUserLogbook, getUserLogbooks, deleteUserLogbook, getUserLogbook } from "../controller/logbookController.js";
 import { protect } from '../middleware/protect.js'
 
 const router = express.Router()
 
 router.post('/logbookEntry', protect, createLogbook)
 router.get('/userLogbooks/:userId', protect, getUserLogbooks)
-router.get('/userLogbook/:entryId', protect, getUserLogbooks)
+router.get('/userLogbook/:entryId', protect, getUserLogbook)
 router.put('/editLogbook/:entryId', protect, updateUserLogbook)
 router.delete('/deleteLogbook/:entryId', protect, deleteUserLogbook)
 
