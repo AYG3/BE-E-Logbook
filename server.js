8e48fa7b1f9e82de4673119eea195bf6d5fa3ee7
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-06 13:01:18 +0100

    refactor: Add cors package to enable Cross-Origin Resource Sharing

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ import express from 'express'
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import logbookRoutes from './routes/logbookRoutes.js'
+
 import dotenv from 'dotenv';
+import cors from 'cors'
+
 
 dotenv.config();
 
+
 const app = express();
+app.use(cors())
 
 connectDB()
 app.use(express.json())
