a9446ee0caf6e9734f1115985d504c6785d18a2a
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-06 13:25:07 +0100

    refactor: Update server.js to use cors package for Cross-Origin Resource Sharing

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import connectDB from './config/db.js'
-import authRoutes from './routes/authRoutes.js'
-import logbookRoutes from './routes/logbookRoutes.js'
+import express from "express";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import logbookRoutes from "./routes/logbookRoutes.js";
 
-import dotenv from 'dotenv';
-import cors from 'cors'
-
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-
 const app = express();
-app.use(cors())
+app.use(cors());
 
-connectDB()
-app.use(express.json())
+connectDB();
+app.use(express.json());
 
-app.use('/auth', authRoutes)
-app.use('/logbook', logbookRoutes)
+app.use("/auth", authRoutes);
+app.use("/logbook", logbookRoutes);
+
+app.listen(4444, () => {
+  console.log("Server running on http://localhost:4444");
+});
 
 export default app;
