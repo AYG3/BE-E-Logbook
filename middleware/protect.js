39897bd33fcdf9b9b91081dd8decd937fc31e1ac
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-08 17:47:09 +0100

    Refactor protect middleware to remove console.log statement and improve error handling

--- middleware/protect.js
+++ middleware/protect.js
@@ -23,7 +23,6 @@ export const protect = async (req, res, next) => {
   }
 
   if (!token) {
-    // console.log('!token error: ', error)
     res.status(401).json({ message: "Not authorized, no token" });
   }
 };
