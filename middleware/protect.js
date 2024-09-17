e6491b7f876d0eb1ce30729012188fc6840ef95e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-17 09:40:18 +0100

    Refactor isAdmin middleware in protect.js: Update console.log statement for debugging

--- middleware/protect.js
+++ middleware/protect.js
@@ -29,7 +29,7 @@ export const protect = async (req, res, next) => {
 
 
 export const isAdmin = (req, res, next) => {
-  console.log(req.user.role)
+  console.log("Request role: ", res.user.role);
   if (req.user &&  req.user.role == 'admin'){
     next();
   }
