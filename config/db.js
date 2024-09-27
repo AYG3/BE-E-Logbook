3f16c0b0f9c1891dec2386e589af24615cf0f71a
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-27 09:00:40 +0100

    Refactor db.js: Update MongoDB connection URL and remove unused variable

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import app from '../server.js';
+import PORT from '../'
 
 
 const PORT = 4444;
-const mongoDBURL = 'mongodb+srv://gilbertayoku3:XZ3mZSCSLI8Jbnnm@cluster0.c997hrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoDBURI = 'mongodb+srv://gilbertayoku3:XZ3mZSCSLI8Jbnnm@cluster0.c997hrt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongoDBURL);
+        await mongoose.connect(mongoDBURI);
         app.listen(PORT, ()=> {
             console.log("Checkers works")
         })
