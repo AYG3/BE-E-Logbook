c3cb71853b211fda0709af26c803b779b5c572e0
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-09-30 10:34:10 +0100

    Refactor User model: Add organization field

--- models/User.js
+++ models/User.js
@@ -29,6 +29,9 @@ const userSchema = new mongoose.Schema({
         enum: ['user', 'admin'],
         default: 'user',
     },
+    organization: {
+
+    },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
 }, {
